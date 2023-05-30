21aa4a25208b3ac53d14c9896a02cd2eea1f5f4a
Fionzera <fionzera@gmail.com>
2023-05-29 23:05:21 -0300

    making some tests

---
+++ index.js
@@ -0,0 +1,14 @@
+const colors = require('colors');
+const express = require('express');
+
+const server = express();
+
+server.get("/", (req, res) =>{
+    res.send("Hello World!");
+})
+
+
+const port = 8080;
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
